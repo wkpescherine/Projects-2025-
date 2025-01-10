a77bb4385ece56891dbf03000fbb28e5f36567b2
wkpescherine <wkpescherine@yahoo.com>
2025-01-09 22:48:27 -0500

    010925 Mercantus(0.5)  : Added API and setup basics

--- marketplace/src/main.js
+++ marketplace/src/main.js
@@ -52,7 +52,7 @@ function Main() {
                 <button class='invertButton' onClick={RedirectUnderDev}>Login</button> 
                 <button class='invertButton' onClick={RedirectToCreateAccount}>Create Account</button>
             </p>
-            <p>version 0.4</p>
+            <p>version 0.5</p>
         </div>
     ); 
 }
