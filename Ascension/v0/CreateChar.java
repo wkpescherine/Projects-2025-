3b7db5644c451615246f2572caab41855fe0414e
wkpescherine <wkpescherine@yahoo.com>
2025-01-17 22:46:16 -0500

    011725 Ascension(0.5.10) :Updated GameConfig sheet to handle creation choices                           Added Char Sheet section on creation screen                           Added Bonus Calculation in GameConfig section                           Added logic for handling selections and data updates on creation screen                           Added New Races to Creation screen                           Added new Roles to Creation screen                             > Have to look further at the bonus calculation as it breaks the UI

--- Ascension/v0/CreateChar.java
+++ Ascension/v0/CreateChar.java
@@ -12,9 +12,16 @@ public class CreateChar implements ActionListener{
     JButton savage = new JButton("Savage");
     JButton wise = new JButton("Wise");
     JButton nimble = new JButton("Nimble");
+    JButton human = new JButton("Human");
+    JButton dwarven = new JButton("Dwarven");
+    JButton warrior = new JButton("Warrior");
+    JButton magi = new JButton("Magi");
     JPanel style = new JPanel();
     JPanel race = new JPanel();
     JPanel role = new JPanel();
+    JLabel sheetStr = new JLabel ("STR:");
+    JLabel sheetDex = new JLabel ("DEX:");
+    JLabel sheetQui = new JLabel ("QUI:");
 
     String buttonName ="";
 
@@ -31,17 +38,42 @@ public class CreateChar implements ActionListener{
                 public void actionPerformed(ActionEvent e) {
                     buttonName = ((JButton) e.getSource()).getText();
                     GameConfig.handleStyleChocie(buttonName);
+                    updateSheet();
                 }
             }); 
+        nimble.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    buttonName = ((JButton) e.getSource()).getText();
+                    GameConfig.handleStyleChocie(buttonName);
+                    updateSheet();
+                }
+            });
 
         //Race selection section
         race.setPreferredSize(new Dimension(600,250));
         race.setBounds(25,225,600,175);
+        race.add(human);
+        race.add(dwarven);
 
         //Role Selection section
         role.setPreferredSize(new Dimension(600,250));
         role.setBounds(25,425,600,175);
+        role.add(warrior);
+        role.add(magi);
 
+        //Char stats sheet
+        sheetStr.setPreferredSize(new Dimension(200,25));
+        sheetStr.setBounds(650, 25,200,25);
+        sheetStr.setForeground(Color.WHITE);
+        sheetDex.setPreferredSize(new Dimension(200,25));
+        sheetDex.setBounds(650, 75,200,25);
+        sheetDex.setForeground(Color.WHITE);
+        sheetQui.setPreferredSize(new Dimension(200,25));
+        sheetQui.setBounds(650, 125,200,25);
+        sheetQui.setForeground(Color.WHITE);
+
+        //Panel Setup info
         exit.setPreferredSize(new Dimension(100, 50));
         exit.setBounds(395, 625, 100, 50);
         play.setPreferredSize(new Dimension(100, 50));
@@ -52,6 +84,9 @@ public class CreateChar implements ActionListener{
         create.add(role);
         create.add(play);
         create.add(exit);
+        create.add(sheetStr);
+        create.add(sheetDex);
+        create.add(sheetQui);
         create.setLayout(null);
         create.setBackground(Color.BLACK);
         create.setPreferredSize(new Dimension(1000, 750));
@@ -60,5 +95,19 @@ public class CreateChar implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         buttonName = ((JButton) e.getSource()).getText();
         GameConfig.handleStyleChocie(buttonName);
+        updateSheet();
+    }
+
+    //public void updateSheet(){
+    //    sheetStr.setText("STR: " + GameConfig.stats[0]);
+    //    sheetDex.setText("DEX: " + GameConfig.stats[1]);
+    //    sheetQui.setText("QUI: " + GameConfig.stats[2]);
+    //}
+
+    public void updateSheet(){
+        sheetStr.setText("STR: " + GameConfig.stats[0] + " + " + GameConfig.statBonus[0]);
+        sheetDex.setText("DEX: " + GameConfig.stats[1] + " + " + GameConfig.statBonus[1]);
+        sheetQui.setText("QUI: " + GameConfig.stats[2] + " + " + GameConfig.statBonus[2]);
     }
 }
+//64
--- Ascension/v0/GameConfig.java
+++ Ascension/v0/GameConfig.java
@@ -9,27 +9,66 @@ public class GameConfig {
     public static int[] styleTempValues= {0,0,0,0,0,0};
     public static int[] raceTempValues = {0,0,0,0,0,0};
     public static int[] roleTempValues = {0,0,0,0,0,0};
-    public static int strength = 0;
-    public static int dexterity = 0;
-    public static int quickness = 0;
-    public static int constitution =0;
-    public static int intelligence =0;
-    public static int piety = 0;
+    public static int[] statBonus      = {0,0,0,0,0,0};
+    public static int[] stats          = {0,0,0,0,0,0};
 
     public static void handleStyleChocie(String choice){
         switch (choice) {
             case "Savage":
                 int[] savageStyle = {3,1,2,0,0,0};
                 styleTempValues = savageStyle;
+                statsTally();
+                //bonusCheck();
                 break;
             case "Wise":
-                int[] wiseStyle = {3,1,2,0,0,0};
+                int[] wiseStyle = {0,1,2,3,0,0};
                 styleTempValues = wiseStyle;
+                statsTally();
+                //bonusCheck();
+                break;
             case "Nimble":
-                int[] nimbleStyle = {3,1,2,0,0,0};
+                int[] nimbleStyle = {0,3,3,0,0,0};
                 styleTempValues = nimbleStyle;
+                statsTally();
+                //bonusCheck();
+                break;
             default:
                 throw new AssertionError();
         }
     }
+
+    public static void handleRaceChoice(String choiceRace){
+        switch (choiceRace) {
+            case "Human":
+                int [] humanRace = {1,1,1,1,1,1};
+                raceTempValues = humanRace;
+                statsTally();
+                //bonusCheck();
+                break;
+            case "Dwarven":
+                int [] dwarvenRace = {2,1,1,0,0,3};
+                raceTempValues = dwarvenRace;
+                statsTally();
+                //bonusCheck();
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    public static void statsTally(){
+        for(int a = 0; a<=5; a++ ){
+            stats[a] = styleTempValues[a]+raceTempValues[a]+roleTempValues[a];
+        }
+    }
+
+    public static void bonusCheck(){
+        for(int b = 0; b <= 5; b++){
+            int bonVal = 0;
+            for(int c = 0; bonVal <= stats[b]; c++){
+                if(bonVal < stats[b]){bonVal = bonVal+c;}
+                else{ statBonus[b]= c-1;}
+            }
+        }
+    }  
 }
+//35
