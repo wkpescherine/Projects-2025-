b5ab93aaf4eb7f165563a1c83a3ee60bd32a0c5d
wkpescherine <wkpescherine@yahoo.com>
2025-01-19 23:54:49 -0500

    011925 Ascension(0.7.12)  :Found issue on bonus calculation                             Bug: Have to look further at the bonus calculation, found issue

--- Ascension/v0/GameConfig.java
+++ Ascension/v0/GameConfig.java
@@ -18,19 +18,19 @@ public class GameConfig {
                 int[] savageStyle = {3,1,2,0,0,0};
                 styleTempValues = savageStyle;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             case "Wise":
                 int[] wiseStyle = {0,1,2,3,0,0};
                 styleTempValues = wiseStyle;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             case "Nimble":
                 int[] nimbleStyle = {0,3,3,0,0,0};
                 styleTempValues = nimbleStyle;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             default:
                 throw new AssertionError();
@@ -43,19 +43,19 @@ public class GameConfig {
                 int [] humanRace = {1,1,1,1,1,1};
                 raceTempValues = humanRace;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             case "Dwarven":
                 int [] dwarvenRace = {2,1,1,0,0,3};
                 raceTempValues = dwarvenRace;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             case "Elven":
                 int [] elvenRace = {0,1,3,2,0,0};
                 raceTempValues = elvenRace;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             default:
                 throw new AssertionError();
@@ -68,19 +68,19 @@ public class GameConfig {
                 int [] warriorRole = {2,0,1,0,0,3};
                 roleTempValues = warriorRole;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             case "Magi":
                 int [] magiRole = {0,1,2,3,0,0};
                 roleTempValues = magiRole;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             case "Rogue":
                 int [] rogueRole = {0,3,3,0,0,0};
                 roleTempValues = rogueRole;
                 statsTally();
-                //bonusCheck();
+                bonusCheck();
                 break;
             default:
                 throw new AssertionError();
@@ -96,11 +96,33 @@ public class GameConfig {
 
     public static void bonusCheck(){
         for(int b = 0; b <= 5; b++){
-            int bonVal = 0;
-            for(int c = 0; bonVal <= stats[b]; c++){
-                if(bonVal < stats[b]){bonVal = bonVal+c;}
-                else{ statBonus[b]= c-1;}
+            System.out.println(b);
+            //int locaStat = b;
+            int bonusTot = 0;
+            //int bonusVal = 0;
+            int statCap = stats[b];
+            System.out.println(statCap);
+            for(int bon = 0; bonusTot <= statCap; bon +=1){
+                System.out.println("here");
+                int statCheck = statCap;
+                int tempValue = bonusTot + bon;
+                if(tempValue < statCheck){ 
+                    System.out.println("here 2");
+                    bonusTot = tempValue;
+                }else{
+                    statBonus[b]= bon;
+                }
             }
+
+            //while(bonusTot < statCap){
+            //    statBonus[locaStat]= bonusVal;
+            //    System.out.println(bonusVal);
+            //    if(bonusTot < statCap){
+            //        bonusTot = bonusTot+bonusVal;
+            //        bonusVal += 1;
+            //    }
+            //}
         }
     }  
 }
+//108
