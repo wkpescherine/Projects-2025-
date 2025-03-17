772c78edfc0574552145a82b6220bae2c5fdeec5
wkpescherine <wkpescherine@yahoo.com>
2025-03-16 23:52:27 -0400

    031625 Mystika(0.1)     : Created Base Activity                         : Added Home Activity

---
+++ Mystika/app/src/main/java/com/example/mystika/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mystika;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void LogIn(View v){
+        Intent Login = new Intent(this, Home.class);
+        startActivity(Login);
+    }
+}
