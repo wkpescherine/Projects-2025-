4e59d920152eace35ada4fe861bdd074b2507f76
wkpescherine <wkpescherine@yahoo.com>
2025-04-14 23:24:03 -0400

    041425 TruCare(0.28)    : Updated Account Screen                         : Split account screen based on Patient/Doctor                         : Adding basic setup for API

--- Trucare/web/v0/trucare/src/main.js
+++ Trucare/web/v0/trucare/src/main.js
@@ -56,7 +56,7 @@ function Main() {
                 <button class='invertButton' onClick={RedirectToPCreate}>Create Patient</button>
                 <button class='invertButton' onClick={RedirectToDCreate}>Create Doctor</button>  
             </p>
-            <p>version 0.27</p>
+            <p>version 0.28</p>
         </div>
     ); 
 }
