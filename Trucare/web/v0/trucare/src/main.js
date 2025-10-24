0a41a5336de6e3198680bfd766bf64af2d63529a
wkpescherine <wkpescherine@yahoo.com>
2025-10-23 23:43:34 -0400

    - Added doctor displaying to the search section - Updated some logic in Search section to handle doctor searching

--- Trucare/web/v0/trucare/src/main.js
+++ Trucare/web/v0/trucare/src/main.js
@@ -56,7 +56,7 @@ function Main() {
                 <button class='invertButton' onClick={RedirectToPCreate}>Create Patient</button>
                 <button class='invertButton' onClick={RedirectToDCreate}>Create Doctor</button>  
             </p>
-            <p>version 0.29.4</p>
+            <p>version 0.30.4</p>
         </div>
     ); 
 }
--- Trucare/web/v0/trucare/src/search.js
+++ Trucare/web/v0/trucare/src/search.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React,{useState}from 'react';
 import './App.css';
+import Doctor from './search_doctor';
 
 function Search() {
+    const[occupation,setOccupation] = useState ("");
+
+    function selectOccupation(occ){
+        if(occ === occupation){selectOccupation("")}
+        else { setOccupation(occ)};
+    }
+    
     return (
         <div style={{marginTop: "25px"}}>
             <p>Search by:</p>
             <div>
-                <label>City:</label>
+                <label style={{marginRight:"2px", fontSize: "12px", width: "140px"}}>Occupation: {occupation} </label>
+                <label style={{marginRight:"2px", fontSize: "12px", width: "80px"}}>City:</label>
                 <input type='text' style={{width: "100px"}}></input>
-                <label>State:</label>
+                <label style={{marginRight:"2px", fontSize: "12px", width: "80px"}}>State:</label>
                 <input type='text' style={{width: "25px"}}></input>
             </div>
             <div>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Family</button>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Dentist</button>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Eye</button>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Allergist</button>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Therapist</button>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Psychiatrist</button>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Trainer</button>
-                    <button style={{marginRight:"2px", fontSize: "12px"}}>Pharmacy</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Family")}>Family</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Dentist")}>Dentist</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Eye")}>Eye</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Allergist")}>Allergist</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Therapist")}>Therapist</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Psychiatrist")}>Psychiatrist</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Trainer")}>Trainer</button>
+                    <button style={{marginRight:"2px", fontSize: "12px"}} onClick={() => selectOccupation("Pharmacy")}>Pharmacy</button>
                 </div>
             <hr style={{background:"#fff", height: "1px", width: "500px"}}/>
             <div style={{display:"flex", justifyContent:"center"}}>
@@ -27,9 +36,10 @@ function Search() {
                 <p style={{width:"100px", fontSize:"12px"}}>City</p>
                 <p style={{width:"50px", fontSize:"12px"}}>State</p>
                 <p style={{width:"100px", fontSize:"12px"}}>Zip Code</p>
-                <p style={{width:"150px", fontSize:"12px"}}>Occupation</p>
+                <p style={{width:"55px", fontSize:"12px"}}></p>
             </div>
             <hr style={{background:"#fff", height: "1px", width: "500px"}}/>
+            <Doctor/>
         </div>
     ); 
 }
